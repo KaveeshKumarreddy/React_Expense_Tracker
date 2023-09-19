bedd70d16d1eeefe9c22d2e5b0d2b993142f2be1
KaveeshKumarreddy <kaveeshkumarreddy@gmail.com>
2023-09-19 22:19:22 +0530

    Udemy Module_3 : React Basic and Working with Components

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,28 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        date={expenses[1].date}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        date={expenses[2].date}
+        amount={expenses[2].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
